5e6f5a57cf9d40d1570647cd8d21efd008af72c7
pvq2k2 <vanquyet2392002@gmail.com>
2023-11-18 04:52:21 +0700

    feat: toast theme sync

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,42 @@
 import { ToastContainer } from "react-toastify";
 import Router from "./Router";
+import { useEffect, useRef, useState } from "react";
+import { useSelector } from "react-redux";
 
 function App() {
+  const [toasTheme, setToastTheme] = useState("light");
+  const theme = useSelector((state) => state.darkMode.mode);
+
+  const prefersDarkMode = useRef(
+    window.matchMedia && window.matchMedia("(prefers-color-scheme: dark)"),
+  );
+
+  useEffect(() => {
+    const updateDarkMode = (event) => {
+      const prefersDark = event.matches;
+      setToastTheme(prefersDark ? "dark" : "light");
+    };
+
+    prefersDarkMode.current.addEventListener("change", updateDarkMode);
+
+    return () => {
+      prefersDarkMode.current.removeEventListener("change", updateDarkMode);
+    };
+  }, [theme == "system"]);
+
+  useEffect(() => {
+    if (theme === "system") {
+      if (prefersDarkMode.current.matches) {
+        setToastTheme("dark");
+      } else {
+        setToastTheme("light");
+      }
+    } else if (theme === "dark") {
+      setToastTheme("dark");
+    } else {
+      setToastTheme("light");
+    }
+  }, [theme]);
   return (
     <>
       <Router />
@@ -15,7 +50,7 @@ function App() {
         pauseOnFocusLoss
         draggable
         pauseOnHover
-        theme="light"
+        theme={toasTheme}
       />
     </>
   );
