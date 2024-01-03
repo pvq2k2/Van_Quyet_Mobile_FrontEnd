137f1e513d50c388bbfde8f2509e6e3f8587f63a
pvq2k2 <vanquyet2392002@gmail.com>
2024-01-03 23:22:04 +0700

    feat: sub categories update

--- src/services/subCategories.js
+++ src/services/subCategories.js
@@ -35,3 +35,31 @@ export const createSubCategories = async (subCategoryData) => {
     throw cathError(error);
   }
 };
+
+export const updateSubCategories = async (subCategoriesData) => {
+  try {
+    const res = await instance.put(
+      `/sub-categories/update-sub-categories/${subCategoriesData.id}`,
+      subCategoriesData.formData,
+      {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+      },
+    );
+    return res && res.data;
+  } catch (error) {
+    throw cathError(error);
+  }
+};
+
+export const getSubCategoriesByID = async (subCategoriesID) => {
+  try {
+    const res = await instance.get(
+      `/sub-categories/get-sub-categories-by-id/${subCategoriesID}`,
+    );
+    return res && res.data;
+  } catch (error) {
+    throw cathError(error);
+  }
+};
